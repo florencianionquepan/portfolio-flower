c5e7d7b83e867b380ebcc2df8acf04b66bcc1795
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-08-25 20:46:55 -0300

    add other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { MyHeader } from './assets/MyHeader'
 
 function App() {
   
 
   return (
     <>
-      
+      <div className='min-h-[800px] flex flex-col'>
+        <MyHeader/>
+      </div>
     </>
   )
 }
