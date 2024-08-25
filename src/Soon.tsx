3a72562edd021b241052622b95cb070cf642d638
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-08-25 20:28:02 -0300

    add soon component

---
+++ src/Soon.tsx
@@ -0,0 +1,9 @@
+
+export const Soon = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-gray-800 text-white pt-[10%]">
+      <h1 className="text-4xl font-bold">Portfolio under construction</h1>
+      <p className="text-lg font-light italic">New updates are coming soon...</p>
+    </div>
+  )
+}
