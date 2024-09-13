4dd23e9dbf069efb515cbd6082952c1903614f81
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-12 21:45:24 -0300

    Create Girl.tsx

---
+++ src/presentation/draw/Girl.tsx
@@ -0,0 +1,22 @@
+import './girl.css';
+
+export const Girl = () => {
+  return (
+    <div className="cartoon-girl">
+      <div className="bangs"></div>
+      <div className='hear hear-l'></div>
+      <div className='hear hear-r'></div>
+      <div className='hear-back'></div>
+
+      <div className='head'>
+        <div className='eye l'></div>
+        <div className='eye r'></div>
+        <div className='nose'></div>
+        <div className='mouth'></div>
+      </div>
+        
+      <div className='neck'></div>
+
+    </div>
+  )
+}
