a128927c276d967fd65966993934e01f8e1665fa
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-14 11:55:48 -0300

    Update Girl.tsx

--- src/presentation/draw/Girl.tsx
+++ src/presentation/draw/Girl.tsx
@@ -3,16 +3,20 @@ import './girl.css';
 export const Girl = () => {
   return (
     <div className="cartoon-girl">
-      <div className='all-hear'>
-        <div className="bangs"></div>
-        <div className='hear hear-l'></div>
-        <div className='hear hear-r'></div>
-        <div className='hear-back'></div>
-      </div>
+      <div className='hear-back'></div>
 
       <div className='head'>
-        <div className='eye l'></div>
-        <div className='eye r'></div>
+
+        <div className="bang-left"></div>
+        <div className="bang-right"></div>
+
+        <div className='glass l'>
+          <div className='eye'></div>
+        </div>
+        <div className='glass r'>
+          <div className='eye'></div>
+        </div>
+        <div className='ear'></div>
         <div className='nose'></div>
         <div className='mouth'></div>
       </div>
