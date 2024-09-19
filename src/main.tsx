2c0e5372d34bf0d8f0e42df2e1dabf859e8fd62c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-19 19:13:26 -0300

    add container

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Soon } from './Soon'
+import { MyHeader } from './assets/MyHeader'
+import { Presentation } from './presentation/Presentation'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Soon/>
+    <div className='container max-w-7xl mx-auto'>
+      <MyHeader/>
+      <Presentation/>
+    </div>
   </React.StrictMode>,
 )
