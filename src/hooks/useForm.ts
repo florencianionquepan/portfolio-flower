8c803a6460cc8b0e9dceabee466104368316f95d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-09-26 21:17:27 -0300

    Create useForm.ts

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,27 @@
+import { useState, ChangeEvent } from "react";
+
+
+//export function useForm<T>(initialForm:T){
+export const useForm = <T extends object> (initialForm:T) => {
+
+    const [formState, setFormState] = useState(initialForm);
+    
+      const onInputChange =({target}: ChangeEvent<HTMLInputElement> ) => {
+        const {name,value} = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+      }
+      
+    const onResetForm =()=>{
+      setFormState(initialForm);
+    } 
+      
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm
+  }
+}
