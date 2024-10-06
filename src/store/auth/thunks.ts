6cc6d5acc46f4bb1ab8e9c145e77e8c97d4b8290
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-06 20:39:37 -0300

    add logoutSpring

--- src/store/auth/thunks.ts
+++ src/store/auth/thunks.ts
@@ -1,5 +1,5 @@
 import { AppDispatch } from "../store";
-import { checkingCredentials } from "./authSlice"
+import { checkingCredentials, logout } from "./authSlice"
 
 export const startSignIn =()=>{
 
@@ -8,4 +8,19 @@ export const startSignIn =()=>{
         //no hago mas nada pq se va a recargar la pagina
     }
 
+}
+
+export const logoutSpring = () =>{
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    return async(dispatch: AppDispatch)=>{
+
+        const resp= await fetch(`${apiUrl}/logout`,{
+            method: 'POST',
+            credentials: 'include'
+        });
+        if(resp.ok){
+            dispatch(logout());
+        }
+    }
 }
