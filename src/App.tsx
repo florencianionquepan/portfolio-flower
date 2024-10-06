a072cca70ed40fd987a30ae59ef42697303caafb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-06 20:40:10 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { MyHeader } from './assets/MyHeader'
+import { Education } from './education/Education'
+import { useAuth } from './hooks/useAuth'
+import { Presentation } from './presentation/Presentation'
 
 function App() {
-  
+
+  useAuth();
 
   return (
-    <>
-      <div className='min-h-[800px] flex flex-col'>
-        <MyHeader/>
+      <div className='container max-w-7xl mx-auto'>
+          <MyHeader/>
+          <Presentation/>
+          <Education/>
       </div>
-    </>
   )
 }
 
