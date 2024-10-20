d4fb14321f21ebc8a1823771ae1f43eb080d2117
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-10-20 19:36:20 -0300

    Create Education.ts

---
+++ src/store/model/Education.ts
@@ -0,0 +1,27 @@
+export class Education {
+    public id: number;
+    public name: string;
+    public institution: string;
+    public degreeType?: string;
+    public startDate: Date;
+    public endDate?: Date;
+    public status: string;
+
+    constructor(
+        id: number,
+        name: string,
+        institution: string,
+        startDate: Date,
+        status: string,
+        degreeType?: string,
+        endDate?: Date
+    ) {
+        this.id = id;
+        this.name = name;
+        this.institution = institution;
+        this.startDate = startDate;
+        this.status = status;
+        this.degreeType = degreeType;
+        this.endDate = endDate;
+    }
+}
