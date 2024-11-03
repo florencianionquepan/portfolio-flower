9cdea4ad78c5644ba8c1876d68ed1e5b90d2a6fb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-03 20:33:48 -0300

    add function to sort educations

--- src/education/Education.tsx
+++ src/education/Education.tsx
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux"
 import { Titles } from "../assets/Titles"
-import { startNewEducation } from "../store/education/thunk"
 import { AppDispatch, RootState } from "../store/store"
 import { EducationForm } from "./EducationForm"
 import { useSelector } from "react-redux"
 import { openNewEducation } from "../store/education/educationSlice"
 import { PlusIcon } from "@heroicons/react/24/outline"
 import { EducationItem } from "./EducationItem"
+import { useMemo } from "react"
 
 
 export const Education = () => {
@@ -14,16 +14,18 @@ export const Education = () => {
   const dispatch: AppDispatch = useDispatch();
   const {educations, loading, isFormOpen} = useSelector( (state: RootState) => state.education)
 
-  //esta funcion va a estar e n el boton de enviar educacion debajo del formulario!!!
-  const onClickNewEducation = () =>{
-    dispatch(startNewEducation());
-
-  }
-
   const handleOpenNewEducation = () =>{
     console.log(educations);
     dispatch( openNewEducation() );
   }
+
+  const sortedEducations = useMemo(() => {
+    return [...educations].sort((a, b) => {
+        const endDateA = new Date(a.startDate).getTime();
+        const endDateB = new Date(b.startDate).getTime();
+        return endDateB - endDateA;
+    });
+}, [educations]);
 
   return (
     <div className="p-8 xl:p-16">
@@ -37,7 +39,7 @@ export const Education = () => {
         </button>
       </div>
       <div>
-        {educations.map((e)=>(
+        {sortedEducations.map((e)=>(
           <EducationItem
           key={e.id} 
           education={e}
