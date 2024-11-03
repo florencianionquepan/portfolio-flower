4f26515f41fcd88679b413f7668136fa3337b961
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-02 21:08:53 -0300

    Create formValidations.ts

---
+++ src/education/formValidations.ts
@@ -0,0 +1,41 @@
+import { EducationFormInterface, FormValidations } from "./educationFormTypes";
+
+export const formValidations: FormValidations<EducationFormInterface> = {
+    name: [
+        (value: string) => /^[A-Za-z\s]+$/.test(value), 
+        'The name can only contain letters and spaces.'
+    ],
+    institution: [
+        (value: string) => /^[A-Za-z\s]+$/.test(value), 
+        'The institution can only contain letters and spaces.'
+    ],
+    degreeType: [
+        (value: string) => /^[A-Za-z\s]*$/.test(value), 
+        'The degree type can only contain letters and spaces.'
+    ],
+    startDate: [
+        (value: Date | null) => {
+            if (value) {
+                const today = new Date();
+                return value < today; 
+            }
+            return false; 
+        },
+        'The start date must be in the past.'
+    ],
+    endDate: [
+        (value: Date | null, status: string) => {
+            if (status === 'PENDING' || status === 'IN PROGRESS') {
+                return true; 
+            }
+            const today = new Date();
+            if(value) return value < today
+            return value !== null; 
+        },
+        'The end date is required unless the status is pending or in progress.'
+    ],
+    status: [
+        (value: string) => !!value, 
+        'Status is required.'
+    ]
+}
