49caf7216c68e859b00d81eb4a20f51403c5f8c5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-09 21:14:19 -0300

    Update Technology.ts

--- src/store/model/Technology.ts
+++ src/store/model/Technology.ts
@@ -1,16 +1,16 @@
 export class Technology{
     public id: number;
     public name: string;
-    public logoURL: string;
+    public logoUrl: string;
     public version: number;
 
     constructor(id?: number, 
                 name?: string, 
-                logoURL?: string, 
+                logoUrl?: string, 
                 version?: number) {
         this.id = id ?? 0;
         this.name = name ?? '';
-        this.logoURL = logoURL ?? '';
+        this.logoUrl = logoUrl ?? '';
         this.version = version ?? 0;
     }
 }
