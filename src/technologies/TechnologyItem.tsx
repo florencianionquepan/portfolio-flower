80e458e1af9061877e5a11373dddd731aed96fe0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-09 21:14:28 -0300

    Update TechnologyItem.tsx

--- src/technologies/TechnologyItem.tsx
+++ src/technologies/TechnologyItem.tsx
@@ -8,8 +8,12 @@ export const TechnologyItem = ({technology}:TechnologyItemsProps) => {
   return (
     <>
         <span className="inline-flex items-center rounded-md bg-purple-50 
-        px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset 
-        ring-purple-700/10">
+        px-1 pr-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset 
+        ring-purple-700 m-2">
+          <img 
+          src={technology.logoUrl} 
+          alt={`${technology.name} logo`} 
+          className="w-4 h-4 mr-1" />
             {technology.name} | v: {technology.version}
         </span>
     </>
