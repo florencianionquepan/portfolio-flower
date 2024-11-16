5e994e6b0a62ac931006e310c89c4c0b5f22fa7d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-15 23:59:03 -0300

    Update TechnologySection.tsx

--- src/technologies/TechnologySection.tsx
+++ src/technologies/TechnologySection.tsx
@@ -6,11 +6,13 @@ import { RootState } from '../store/store';
 import { useEffect } from 'react';
 import { showTechnologies } from '../store/technology/thunk';
 import { TechnologyItem } from './TechnologyItem';
+import { openNewTechnology } from '../store/technology/technologySlice';
+import { TechnologyForm } from './TechnologyForm';
 
 export const TechnologySection = () => {
 
     const dispatch = useAppDispatch();
-    const {technologies} = useSelector( (state: RootState) => state.technology);
+    const {technologies, loading, isFormOpen} = useSelector( (state: RootState) => state.technology);
 
     useEffect(() => {
       dispatch(showTechnologies());
@@ -19,15 +21,17 @@ export const TechnologySection = () => {
     
 
     const handleOpenNewTechnology = ()=>{
-    
+      dispatch(openNewTechnology());
     }
+
       return (
         <div className="p-8 xl:px-16">
           <div className="flex items-center justify-between mb-4">
             <Titles title="Technologies"/>
             <button className="bg-transparent border border-2 border-purple-600 
             rounded-full hover:shadow hover:shadow-purple-600 disabled:opacity-25 p-1"
-            onClick={handleOpenNewTechnology} >
+            onClick={handleOpenNewTechnology} 
+            disabled= {loading || isFormOpen}>
               <PlusIcon className="h-5 w-5 text-purple-600"></PlusIcon>
             </button>
           </div>
@@ -39,6 +43,7 @@ export const TechnologySection = () => {
               />
             ))}
           </div>
+          {isFormOpen && <TechnologyForm/>}
         </div>
       )
 }
