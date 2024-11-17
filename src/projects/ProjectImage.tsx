1477671610a35bb102d5965939e87706cef5547e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-11-16 22:40:28 -0300

    Update ProjectImage.tsx

--- src/projects/ProjectImage.tsx
+++ src/projects/ProjectImage.tsx
@@ -4,7 +4,7 @@ interface ProjectImageProps {
 
 export const ProjectImage: React.FC<ProjectImageProps> = ({image}) => {
   return (
-    <div className="max-h-[300px] overflow-y-auto border border-8 rounded border-gray-800">
+    <div className="max-h-[300px] overflow-y-auto border border-8 rounded border-gray-800 m-2">
         <img 
             src={image} 
             alt="Scrollable content" 
