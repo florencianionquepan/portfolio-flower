109e5d249c1309454367ecd858450cc4c6bd71f0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-01-16 20:00:09 -0300

    implement carousel for images of projects

--- src/projects/ProjectItem.tsx
+++ src/projects/ProjectItem.tsx
@@ -1,8 +1,8 @@
 import { ProjectLinks } from "./ProjectLinks";
-import { ProjectImage } from "./ProjectImage";
 import { Project } from "../store/model/Project";
 import React from "react";
 import { TechnologyItem } from "../technologies/TechnologyItem";
+import { CarouselImages } from "./CarouselImages";
 
 interface ProjectItemProps{
     project:Project
@@ -12,16 +12,17 @@ export const ProjectItem = ({project}: ProjectItemProps) => {
 
   return (
     <div className='flex max-w-2xl flex-col items-start justify-between 
-          border rounded border-purple-600 shadow-lg shadow-purple-400 m-2 px-2'>
+          border rounded border-purple-600 shadow-lg shadow-purple-400 m-2 '>
 
         <div className="mx-auto">
-          <h3 className="my-2 text-lg font-semibold">{project.title}</h3>
-          <ProjectImage image={project.images![0].url!} />
-          <p className="mt-2">
+          <h3 className="my-2 text-lg font-semibold px-2">{project.title}</h3>
+          <CarouselImages 
+            images={project.images}/>
+          <p className="mt-2 px-1">
               {project.description}
           </p>
 
-          <div className="mt-2 flex flex-wrap gap-2">
+          <div className="mt-2 flex flex-wrap gap-2 px-2">
             {project.links?.map((link, index) => (
               <React.Fragment key={link.id}>
                 {index > 0 && <span className="mx-1"> | </span>}
@@ -32,7 +33,7 @@ export const ProjectItem = ({project}: ProjectItemProps) => {
 
         </div>
 
-        <div className="my-2">
+        <div className="m-2">
             <h6 className="text-black font-normal">Technologies</h6>
             {project.technologies?.map((tech)=>(
               <TechnologyItem
