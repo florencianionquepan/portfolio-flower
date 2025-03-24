f3aa5730ddd7d2efeff20206c809856e75857a52
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-24 19:06:58 -0300

    use creationButton in sections

--- src/education/EducationSection.tsx
+++ src/education/EducationSection.tsx
@@ -4,9 +4,9 @@ import { AppDispatch, RootState } from "../store/store"
 import { EducationForm } from "./EducationForm"
 import { useSelector } from "react-redux"
 import { openNewEducation } from "../store/education/educationSlice"
-import { PlusIcon } from "@heroicons/react/24/outline"
 import { EducationItem } from "./EducationItem"
 import { useMemo } from "react"
+import { CreationButton } from "../assets/CreationButton"
 
 
 export const EducationSection = () => {
@@ -31,12 +31,10 @@ export const EducationSection = () => {
     <div className="p-8 xl:px-16" id="education">
       <div className="flex items-center justify-between mb-4">
         <Titles title="Education"/>
-        <button className="bg-transparent border border-2 border-purple-600 
-        rounded-full hover:shadow hover:shadow-purple-600 disabled:opacity-25 p-1"
+        <CreationButton
         onClick={handleOpenNewEducation}
-        disabled={loading || isFormOpen}>
-          <PlusIcon className="h-5 w-5 text-purple-600"></PlusIcon>
-        </button>
+        disabled={loading || isFormOpen}
+        />
       </div>
       <div>
         {sortedEducations.map((e)=>(
--- src/projects/ProjectSection.tsx
+++ src/projects/ProjectSection.tsx
@@ -1,11 +1,11 @@
 import { useSelector } from 'react-redux';
 import { Titles } from '../assets/Titles'
-import { PlusIcon } from '@heroicons/react/24/outline'
 import { AppDispatch, RootState } from '../store/store';
 import { ProjectItem } from './ProjectItem';
 import { ProjectForm } from './ProjectForm';
 import { useDispatch } from 'react-redux';
 import { openNewProject } from '../store/project/projectSlice';
+import { CreationButton } from '../assets/CreationButton';
 
 export const ProjectSection = () => {
 
@@ -19,13 +19,13 @@ const handleOpenNewProject = ()=>{
     <div className="p-8 xl:px-16" id="projects">
       <div className="flex items-center justify-between mb-4">
         <Titles title="Projects"/>
-        <button className="bg-transparent border border-2 border-purple-600 
-        rounded-full hover:shadow hover:shadow-purple-600 disabled:opacity-25 p-1"
-        onClick={handleOpenNewProject} 
-        disabled= {loading || isFormOpen} >
-          <PlusIcon className="h-5 w-5 text-purple-600"></PlusIcon>
-        </button>
+        <CreationButton
+        onClick={handleOpenNewProject}
+        disabled={loading || isFormOpen} />
       </div>
+      {isFormOpen && 
+        <ProjectForm projectToEdit={projectToEdit}/>
+      }
       <div>
         <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
           
@@ -38,9 +38,7 @@ const handleOpenNewProject = ()=>{
 
         </div>
       </div>
-      {isFormOpen && 
-        <ProjectForm projectToEdit={projectToEdit}/>
-      }
+      
     </div>
   )
 }
