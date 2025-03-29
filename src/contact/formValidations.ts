dbe13b066a61993378a3159113060cf5550c2c3f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-29 17:25:03 -0300

    Create formValidations.ts

---
+++ src/contact/formValidations.ts
@@ -0,0 +1,17 @@
+import { FormValidations } from "../education/educationFormTypes";
+import { ContactFormData } from "./ContactData";
+
+export const formValidations: FormValidations<ContactFormData> = {
+    email: [
+        (value: string) => /\S+@\S+\.\S+/.test(value), 
+        'The email is not valid'
+    ],
+    subject: [
+        (value: string) => /^[A-Za-z\s]+$/.test(value), 
+        'The subject can only contain letters and spaces.'
+    ],
+    description: [
+        (value: string) => /^[A-Za-z0-9\s.,]+$/.test(value),
+        'The description can only contain letters, numbers, spaces, commas, and periods.'
+    ]
+}
