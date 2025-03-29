5e039d4cc7eeb85b7da6f322a36ebfeb8b55b79a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-29 17:12:08 -0300

    Create ContactForm.tsx

---
+++ src/contact/ContactForm.tsx
@@ -0,0 +1,83 @@
+import { Button } from "@headlessui/react"
+import { useForm } from "../hooks/useForm"
+import { InputField } from "../formFields/InputField"
+import { ContactFormData } from "./ContactData"
+import { formValidations } from "./formValidations"
+import { useState } from "react"
+import { TextAreaField } from "../formFields/TextAreaField"
+
+export const ContactForm= () => {
+    const initialFormState: ContactFormData = {
+        email: "",
+        subject: "",
+        description: "",
+    }
+
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    const {email, subject, description, onInputChange, formState, 
+        emailValid, subjectValid, descriptionValid, isFormValid} 
+    = useForm<ContactFormData>(initialFormState, formValidations)
+
+    const onSubmit = (event: React.SyntheticEvent) =>{
+        event.preventDefault();
+        setFormSubmitted(true);
+        if(!isFormValid) return;
+    }
+
+  return (
+    <form autoComplete="off" onSubmit={onSubmit}>
+      <div className="space-y-12 w-full max-w-4xl mx-auto">
+        <div className="border rounded border-2 border-purple-600 p-5 mt-5 bg-orange-200">
+
+          <div className="mt-5 grid grid-cols-1 md:grid-cols-3 gap-3">
+            <div className="md:col-span-3 ml-2">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                <div className="col-span-1">
+                <InputField
+                    name="email"
+                    label="Email"
+                    value={email}
+                    onChange={onInputChange} 
+                    hasError={!!emailValid && formSubmitted}
+                    errorMessage={emailValid}
+                    />
+                </div>
+
+                <div className="col-span-1">
+                <InputField
+                    name="subject"
+                    label="Subject"
+                    value={subject}
+                    onChange={onInputChange} 
+                    hasError={!!subjectValid && formSubmitted}
+                    errorMessage={subjectValid}
+                    />
+                </div>
+
+                <div className="col-span-1 md:col-span-2">
+                  <TextAreaField
+                    label="Description"
+                    name="description"
+                    value={description}
+                    onChange={onInputChange}
+                    hasError={!!descriptionValid && formSubmitted}
+                    errorMessage={descriptionValid}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-8 flex justify-center">
+                <button type="submit" className="border rounded border-2 border-purple-600 p-1 px-2">
+                    Send Message
+                </button>
+           </div>
+
+        </div>
+
+      </div>
+    </form>
+  )
+}
