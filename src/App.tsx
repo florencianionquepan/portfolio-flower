c9cc4d889280420e71878c0e58c7827b81df98df
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-29 17:26:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Presentation } from './presentation/Presentation'
 import { ProjectSection } from './projects/ProjectSection'
 import { RootState } from './store/store'
 import { TechnologySection } from './technologies/TechnologySection'
+import { ContactSection } from './contact/ContactSection'
 
 function App() {
 
@@ -14,13 +15,19 @@ function App() {
   const { status, role } = useSelector((state: RootState) => state.auth);
 
   return (
-      <div className='container max-w-7xl mx-auto'>
-          <MyHeader/>
-          <Presentation/>
-          <EducationSection/>
-          <ProjectSection/>
-          {status === 'auth' && role.includes('ROLE_ADMIN') && <TechnologySection/>}
+    <>
+      <div className='min-h-[800px] flex flex-col'>
+        <MyHeader/>
+        <div className='container max-w-7xl mx-auto'>
+            <MyHeader/>
+            <Presentation/>
+            <EducationSection/>
+            <ProjectSection/>
+            {status === 'auth' && role.includes('ROLE_ADMIN') && <TechnologySection/>}
+            <ContactSection/>
+        </div>
       </div>
+    </>
   )
 }
 
