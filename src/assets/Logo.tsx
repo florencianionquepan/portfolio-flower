88545b49da52e8033d037d7718a2d56119b5846b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-29 17:24:58 -0300

    Create Logo.tsx

---
+++ src/assets/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img src="/dibujo.svg" alt="Logo" className="w-20 h-20" />
+    </div>
+  );
+};
