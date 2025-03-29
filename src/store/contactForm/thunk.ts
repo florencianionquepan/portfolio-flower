b919dfdc4ce84c0b0176604c73209bee62aa5e32
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-03-29 19:10:52 -0300

    Create thunk.ts

---
+++ src/store/contactForm/thunk.ts
@@ -0,0 +1,38 @@
+import Swal from "sweetalert2";
+import { ContactFormData } from "../../contact/ContactData"
+import { AppDispatch } from "../store";
+import { contactSent, errorContactForm, sendingContactData } from "./contactFormSlice";
+import axios, { AxiosError } from "axios";
+
+export const startSendingContactForm = (contact: ContactFormData)=>{
+    return async(dispatch: AppDispatch ) =>{
+
+        const apiUrl = import.meta.env.VITE_API_URL;
+
+        dispatch(sendingContactData());
+
+        const url=`${apiUrl}/public/contact`;
+        try{
+            const resp = await axios.post(url, contact);
+            const message = resp.data;
+
+            dispatch(contactSent());
+            Swal.fire({
+                icon:'success',
+                title:'Success!',
+                text:message
+            })
+    
+        }catch(error){
+            console.log(error);
+            const axiosError = error as AxiosError;
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Error!',
+                text: axiosError? axiosError.message : 'An unexpected error occurred.'
+            });
+            dispatch(errorContactForm(axiosError?.message || ''));
+        }
+    }
+}
