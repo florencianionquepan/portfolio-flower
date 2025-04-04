3754867e74647369219dcd81f14796e038541805
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-04-04 20:34:07 -0300

    Create thunk.ts

---
+++ src/store/resume/thunk.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+import { AppDispatch } from "../store";
+import { downloadError, downloadSuccess, startDownload } from "./resumeSlice";
+
+export const startDownloadingResume = () => {
+    return async (dispatch: AppDispatch) => {
+      dispatch(startDownload());
+  
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/public/resume`, {
+            responseType: 'blob',
+          });
+    
+          const blob = new Blob([response.data], { type: 'application/pdf' });
+          const url = window.URL.createObjectURL(blob);
+    
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = 'resume.pdf';
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);
+          dispatch(downloadSuccess());
+      } catch (error) {
+        console.log(error);
+        const axiosError = error as AxiosError;
+
+        dispatch(downloadError(axiosError?.message || ''));
+      }
+    };
+  };
+  
