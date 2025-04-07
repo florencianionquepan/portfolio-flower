67314080c3dffe3c841050716a64f3f9b0b30ee8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-04-07 19:16:15 -0300

    Update Logo.tsx

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -1,7 +1,21 @@
-export const Logo = () => {
+interface LogoProps {
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+}
+
+export const Logo = ({size='md'}:LogoProps) => {
+
+  const sizeClasses = {
+    sm: 'w-12 h-12',
+    md: 'w-20 h-20',
+    lg: 'w-28 h-28',
+    xl: 'w-44 h-44',
+  };
+
   return (
     <div className="flex justify-center items-center">
-      <img src="/dibujo.svg" alt="Logo" className="w-20 h-20" />
+      <img src="/dibujo.svg" 
+        alt="Logo" 
+        className={`${sizeClasses[size]} transition-all duration-500`}/>
     </div>
   );
 };
