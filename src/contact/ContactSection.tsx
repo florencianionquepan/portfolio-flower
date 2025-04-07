32106c57f30e10ba78c07eac508d69e1f61e0177
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2025-04-07 17:39:38 -0300

    Update ContactSection.tsx

--- src/contact/ContactSection.tsx
+++ src/contact/ContactSection.tsx
@@ -3,7 +3,7 @@ import { ContactForm } from './ContactForm'
 
 export const ContactSection = () => {
   return (
-    <div className="p-8 xl:px-16" id="education">
+    <div className="p-8 xl:px-16" id="contacts">
         <div className="flex items-center justify-between mb-4">
             <Titles title='Contact Me!'/>
         </div>
